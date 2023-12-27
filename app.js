fb6268fba5292566994dd133f44e8854ee4feb4c
Suraj Shelke <surajshelke332@gmail.com>
2023-12-28 04:53:04 +0530

    new Day

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ dotenv.config({
 
 app.use(
   cors({
-    origin:[process.env.FRONTEND_URI],
+    origin:[process.env.FRONTEND_URL],
     methods:["GET",'PUT','DELETE','POST'],
     credentials:true
 
