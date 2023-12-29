9c506c32d7c27eb7561077e32104d825932d662c
Suraj Shelke <surajshelke332@gmail.com>
2023-12-30 01:18:10 +0530

    DataBase Updated !!

--- data/database.js
+++ data/database.js
@@ -4,6 +4,8 @@ const connectDB = () => {
   mongoose
     .connect(process.env.DATABASE_URI, {
       dbName: "TODO_List",
+      useNewUrlParser: true,
+      useUnifiedTopology: true 
     })
     .then((c) => console.log(`Database Connected wit ${c.connection.host}`))
     .catch((e) => console.log(e));
